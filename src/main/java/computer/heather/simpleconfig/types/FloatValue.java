848c105d0577aa12855de88e4c1c681079eec5da
Heather White <heather@heather.computer>
2025-01-18 22:05:55 +0000

    Float config type

---
+++ src/main/java/computer/heather/simpleconfig/types/FloatValue.java
@@ -0,0 +1,58 @@
+package computer.heather.simpleconfig.types;
+
+import computer.heather.simpleconfig.exceptions.validation.BaseValidationException;
+import computer.heather.simpleconfig.exceptions.validation.InvalidTypeException;
+import computer.heather.simpleconfig.exceptions.validation.MissingValueException;
+import computer.heather.simpleconfig.exceptions.validation.OutOfRangeException;
+import computer.heather.simpleconfig.managers.IConfigManager;
+
+
+/**
+ * A FloatValue config type.
+ */
+public class FloatValue extends BaseConfigType<Float> {
+
+    private float value;
+    private float min;
+    private float max;
+    private String range;
+
+    public FloatValue(String key, float defaultValue, float min, float max, IConfigManager manager) {
+        super(key, manager);
+        this.value = defaultValue;
+        this.min = min;
+        this.max = max;
+        this.range = min + "-" + max;  
+    }
+
+    @Override
+    public void validate(String in) throws BaseValidationException {       
+        if (in == null) throw new MissingValueException(this.key);     
+        float i;
+        try {
+            i = Float.parseFloat(in);
+        } catch (NumberFormatException e) {
+            throw new InvalidTypeException("float", in);
+        }
+
+        if (i < min || i > max) throw new OutOfRangeException(range, in);
+
+        return;
+    }
+
+    @Override
+    public Float get() {
+        return value;
+    }
+
+    @Override
+    public void load(String in) {
+        value = Float.parseFloat(in);
+    }
+
+    @Override
+    public String save() {
+        return Float.toString(value);
+    }
+    
+}
