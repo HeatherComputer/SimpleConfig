a6f5b7cdd170d6670a7a53c45e796868026c087d
Heather White <heather@heather.computer>
2025-01-18 00:03:55 +0000

    document ValidationErrorHandler

--- src/main/java/computer/heather/simpleconfig/util/ValidationErrorHandler.java
+++ src/main/java/computer/heather/simpleconfig/util/ValidationErrorHandler.java
@@ -1,7 +1,12 @@
 package computer.heather.simpleconfig.util;
 
+import java.util.function.BiConsumer;
+
 import computer.heather.simpleconfig.exceptions.validation.BaseValidationException;
 
+/**
+ * A functional interface that replicates a {@link BiConsumer}, but can throw {@link BaseValidationException}.
+ */
 public interface ValidationErrorHandler<T, U> {
     void accept(T t, U u) throws BaseValidationException;
 }
