543f8102a873ef0b6b8c4e92921a6344613f03fb
Heather White <heather@heather.computer>
2025-01-18 22:20:58 +0000

    FreeStringValue - accepts any string

---
+++ src/main/java/computer/heather/simpleconfig/types/FreeStringValue.java
@@ -0,0 +1,42 @@
+package computer.heather.simpleconfig.types;
+
+import computer.heather.simpleconfig.exceptions.validation.BaseValidationException;
+import computer.heather.simpleconfig.exceptions.validation.MissingValueException;
+import computer.heather.simpleconfig.managers.IConfigManager;
+
+
+/**
+ * A string config type that accepts any string.
+ */
+public class FreeStringValue extends BaseConfigType<String> {
+
+    private String value;
+
+    public FreeStringValue(String key, String defaultValue, IConfigManager manager) {
+        super(key, manager);
+        this.value = defaultValue;
+    }
+
+    @Override
+    public void validate(String in) throws BaseValidationException {       
+        if (in == null) throw new MissingValueException(this.key);     
+
+        return;
+    }
+
+    @Override
+    public String get() {
+        return value;
+    }
+
+    @Override
+    public void load(String in) {
+        value = in;
+    }
+
+    @Override
+    public String save() {
+        return value;
+    }
+    
+}
