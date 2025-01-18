d78a90fdac2f685f10c05e4acc1d98ee8d7cacf1
Heather White <heather@heather.computer>
2025-01-18 22:09:31 +0000

    Boolean config type

---
+++ src/main/java/computer/heather/simpleconfig/types/BooleanValue.java
@@ -0,0 +1,43 @@
+package computer.heather.simpleconfig.types;
+
+import computer.heather.simpleconfig.exceptions.validation.BaseValidationException;
+import computer.heather.simpleconfig.exceptions.validation.InvalidTypeException;
+import computer.heather.simpleconfig.exceptions.validation.MissingValueException;
+import computer.heather.simpleconfig.managers.IConfigManager;
+
+
+/**
+ * A BooleanValue config type.
+ */
+public class BooleanValue extends BaseConfigType<Boolean> {
+
+    private boolean value;
+
+    public BooleanValue(String key, boolean defaultValue, IConfigManager manager) {
+        super(key, manager);
+        this.value = defaultValue;
+    }
+
+    @Override
+    public void validate(String in) throws BaseValidationException {       
+        if (in == null) throw new MissingValueException(this.key);     
+
+        if ((!in.toLowerCase().equals("false")) || (!in.toLowerCase().equals("true"))) throw new InvalidTypeException("boolean", in);
+    }
+
+    @Override
+    public Boolean get() {
+        return value;
+    }
+
+    @Override
+    public void load(String in) {
+        value = Boolean.parseBoolean(in);
+    }
+
+    @Override
+    public String save() {
+        return Boolean.toString(value);
+    }
+    
+}
