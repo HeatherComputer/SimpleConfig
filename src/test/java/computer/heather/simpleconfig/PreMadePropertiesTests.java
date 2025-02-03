7b0d53f5097df1f0cb2611ce9c10e1c9d5d608b8
Heather White <heather@heather.computer>
2025-02-03 19:09:00 +0000

    cleanup

--- src/test/java/computer/heather/simpleconfig/PreMadePropertiesTests.java
+++ src/test/java/computer/heather/simpleconfig/PreMadePropertiesTests.java
@@ -1,11 +1,7 @@
 package computer.heather.simpleconfig;
 
-import java.io.FileNotFoundException;
-import java.nio.file.Path;
-
 import computer.heather.simpleconfig.managers.PremadePropertiesManager;
 
-@SuppressWarnings("unused")
 class PreMadePropertiesTests extends GenericManagerTests{
 
     @Override
